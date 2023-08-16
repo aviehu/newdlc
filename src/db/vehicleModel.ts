ab63558ff1ca1462319d089087b2491ccae721d0
aviehu <aviehu@gmail.com>
2023-08-16 15:09:50 +0300

    no groups and camelCase

--- src/db/vehicleModel.ts
+++ src/db/vehicleModel.ts
@@ -5,6 +5,6 @@ import db from "./dbSetup";
 import {inArray} from "drizzle-orm";
 
 //index by group?
-export default function findAllVehicles(groups: string[]): Promise<Vehicle[]> {
-    return db.select().from(vehicles).where(inArray(vehicles.group, groups))
+export default function findAllVehicles(): Promise<Vehicle[]> {
+    return db.select().from(vehicles)
 }
--- src/db/videopipelineModel.ts
+++ src/db/videopipelineModel.ts
@@ -11,96 +11,91 @@ interface Overrides {
 
 //should everything be indexed by group?
 
-export async function insertVideoPipeline(newVideoPipeline: NewVideoPipeline, groups: string[]) : Promise<VideoPipeline | undefined> {
-    if(!groups.includes(newVideoPipeline.group)) {
-        return undefined
-    }
+export async function insertVideoPipeline(newVideoPipeline: NewVideoPipeline) : Promise<VideoPipeline | undefined> {
     const [videoPipeline] = await db.insert(videoPipelines).values(newVideoPipeline).returning()
     return videoPipeline
 }
 
 //index by relay_uuid
-export async function findOneByRelay(relayUuid: string, groups: string[]) : Promise<VideoPipeline | undefined> {
+export async function findOneByRelay(relayUuid: string) : Promise<VideoPipeline | undefined> {
     const [videoPipeline] = await db.select().from(videoPipelines)
-        .where(and(eq(videoPipelines.relay_uuid, relayUuid), inArray(videoPipelines.group, groups)))
+        .where(eq(videoPipelines.relayUuid, relayUuid))
     return videoPipeline
 }
 
 //index by streamer_uuid
-export async function findOneByStreamer(streamerUuid: string, groups: string[]) : Promise<VideoPipeline> {
+export async function findOneByStreamer(streamerUuid: string) : Promise<VideoPipeline> {
     const [videoPipeline] = await db.select().from(videoPipelines)
-        .where(and(eq(videoPipelines.streamer_uuid, streamerUuid), inArray(videoPipelines.group, groups)))
+        .where(eq(videoPipelines.streamerUuid, streamerUuid))
     return videoPipeline
 }
 
 //index by relay_uuid
-
-export async function deleteOneByRelay(relayUuid: string, groups: string[]) : Promise<VideoPipeline | undefined> {
+export async function deleteOneByRelay(relayUuid: string) : Promise<VideoPipeline | undefined> {
     const [videoPipeline] = await db.delete(videoPipelines)
-        .where(and(eq(videoPipelines.relay_uuid, relayUuid), inArray(videoPipelines.group, groups))).returning()
+        .where(eq(videoPipelines.relayUuid, relayUuid)).returning()
     return videoPipeline
 }
 
 //index by relay_uuid
-export async function updateOverrides(relayUuid: string, groups: string[], overrides: Overrides): Promise<VideoPipeline | undefined> {
+export async function updateOverrides(relayUuid: string, overrides: Overrides): Promise<VideoPipeline | undefined> {
     const {allowAutomaticDeallocate, allowAutomaticAllocate} = overrides
     const [videoPipeline] = await db.update(videoPipelines).set({
         allowAutomaticAllocate,
         allowAutomaticDeallocate
-    }).where(and(eq(videoPipelines.relay_uuid, relayUuid), inArray(videoPipelines.group, groups))).returning()
+    }).where(eq(videoPipelines.relayUuid, relayUuid)).returning()
     return videoPipeline
 }
 
 //index by streamer_uuid & index by machineIdentifier
-export function findVideoPipelines(groups: string[], assigned: boolean, machineIdentifier?: string): Promise<VideoPipeline[]> {
-    const whereQuery = [inArray(videoPipelines.group, groups)]
+export function findVideoPipelines(assigned: boolean, machineIdentifier?: string): Promise<VideoPipeline[]> {
+    const whereQuery = [eq(videoPipelines.relayUuid, videoPipelines.relayUuid)]
     if(assigned) {
-        whereQuery.push(not(eq(videoPipelines.streamer_uuid, null)))
+        whereQuery.push(not(eq(videoPipelines.streamerUuid, null)))
     }
     if(machineIdentifier) {
-        whereQuery.push(eq(videoPipelines.machine_identifier, machineIdentifier))
+        whereQuery.push(eq(videoPipelines.machineIdentifier, machineIdentifier))
     }
     return db.select().from(videoPipelines).where(and(...whereQuery))
 }
 
 //index by streamer_uuid & relay_version & relay_set & allowAutomaticAllocate
-export async function assignStreamerToVideoPipeline(streamerUuid: string, version: string, nativeNode: boolean, groups: string[], relaySets: string[]) : Promise<VideoPipeline | undefined> {
-    const existingVideoPipeline = await findOneByStreamer(streamerUuid, groups)
+export async function assignStreamerToVideoPipeline(streamerUuid: string, version: string, nativeNode: boolean, relaySets: string[]) : Promise<VideoPipeline | undefined> {
+    const existingVideoPipeline = await findOneByStreamer(streamerUuid)
     if (existingVideoPipeline) {
         return existingVideoPipeline
     }
     const availableVideoPipelines = await db.select().from(videoPipelines).where(and(
-        eq(videoPipelines.streamer_uuid, null),
-        eq(videoPipelines.relay_version, version),
+        eq(videoPipelines.streamerUuid, null),
+        eq(videoPipelines.relayVersion, version),
         eq(videoPipelines.allowAutomaticAllocate, true),
-        inArray(videoPipelines.relay_set, relaySets),
-        inArray(videoPipelines.group, groups)
+        inArray(videoPipelines.relaySet, relaySets),
         // eq(videoPipelines.relay_status, 'online'),
     ))
     //need to sort
     const natives = availableVideoPipelines.filter((videoPipeline) => {
-        return videoPipeline.node_uuid === null
+        return videoPipeline.nodeUuid === null
     })
     const nonNatives = availableVideoPipelines.filter((videoPipeline) => {
-        return videoPipeline.node_uuid !== null
+        return videoPipeline.nodeUuid !== null
     })
     const selectedNative = relaySets.map((relaySet) => {
         return natives.find((videoPipeline) => {
-            return relaySet === videoPipeline.relay_set
+            return relaySet === videoPipeline.relaySet
         })
     }).find((videoPipeline) => !!videoPipeline)
     const selectedNonNative = relaySets.map((relaySet) => {
         return nonNatives.find((videoPipeline) => {
-            return relaySet === videoPipeline.relay_set
+            return relaySet === videoPipeline.relaySet
         })
     }).find((videoPipeline) => !!videoPipeline)
     return nativeNode ? selectedNative || selectedNonNative : selectedNonNative
 }
 
 // index by relay_uuid
-export async function releaseVideoPipeline(relayUuid: string, groups: string[]) : Promise<VideoPipeline | undefined> {
+export async function releaseVideoPipeline(relayUuid: string) : Promise<VideoPipeline | undefined> {
     const [videoPipeline] = await db.update(videoPipelines).set({
-        node_uuid: null,
-    }).where(and(eq(videoPipelines.relay_uuid, relayUuid), inArray(videoPipelines.group, groups))).returning()
+        nodeUuid: null,
+    }).where(eq(videoPipelines.relayUuid, relayUuid)).returning()
     return videoPipeline
 }
